d165c8850d9185501c78dc0cfade89ad917ea469
AntonioContreras60 <jlantoniocontreras@gmail.com>
2025-07-15 20:28:42 +0200

    ggargae
    agqherheh

--- .gitignore/conexion.php
+++ .gitignore/conexion.php
@@ -10,10 +10,10 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 // Configuración de la base de datos desde variables de entorno
-$servername = getenv('DB_HOST');
-$username   = getenv('DB_USER');
-$password   = getenv('DB_PASS');
-$dbname     = getenv('DB_NAME');
+$servername = "db5016197746.hosting-data.io";
+$username   = "dbu4085097";
+$password   = "123intertruckerya";
+$dbname     = "dbs13181300";
 
 // Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
